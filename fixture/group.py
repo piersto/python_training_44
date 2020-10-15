b60dc0e8350728fe17f0fb35dac878f2c658f44c
piersto <pstoiko@gmail.com>
2020-10-15 13:52:40 -0400

    Added assert to the test_add_group.py

--- fixture/group.py
+++ fixture/group.py
@@ -1,7 +1,20 @@
+from model.group import Group
+
+
 class GroupHelper:
 
     def __init__(self, app):
         self.app = app
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector('span.group'):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute('value')
+            groups.append(Group(name=text, id=id))
+        return groups
 
     def return_to_groups_page(self):
         wd = self.app.wd
@@ -34,6 +47,7 @@ class GroupHelper:
         wd = self.app.wd
         if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name('new')) > 0):
             wd.find_element_by_link_text("groups").click()
+
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
@@ -57,7 +71,6 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         # return to group page
         self.return_to_groups_page()
-
 
     def count(self):
         wd = self.app.wd
