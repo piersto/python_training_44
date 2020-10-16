c96d4818d81b5e127fc1de4570cf7a7e61a401ab
piersto <pstoiko@gmail.com>
2020-10-16 15:02:30 -0400

    4.10: Сортировка списков перед сравнением. Extracted method id_or_max

--- model/group.py
+++ model/group.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Group:
     def __init__(self, name=None, header=None, footer=None, id=None):
         self.name = name
@@ -15,3 +18,8 @@ class Group:
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
 
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
