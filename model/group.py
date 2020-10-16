1276a9638f0745a14247e6b07eec3486cec7fe1c
piersto <pstoiko@gmail.com>
2020-10-16 14:52:08 -0400

    4.10: Сортировка списков перед сравнением. Finished test_add_group.py

--- model/group.py
+++ model/group.py
@@ -13,5 +13,5 @@ class Group:
     Теперь наши объекты будут сравниваться при помощи этой функции, то есть будет выполняться
     сравнение по смыслу, а не по физическому расположению объектов.'''
     def __eq__(self, other):
-        return self.id == other.id and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+from sys import maxsize
 
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create_group(Group(name='Name group', header='Group header', footer='Group footer'))
+    group = Group(name='Name group', header='Group header', footer='Group footer')
+    app.group.create_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+
+    def id_or_max(gr):
+        if gr.id:
+            return int(gr.id)
+        else:
+            return maxsize
+    assert sorted(old_groups, key=id_or_max) == sorted(new_groups, key=id_or_max)
 
 
 def test_add_empty_group(app):
