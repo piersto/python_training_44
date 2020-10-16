d74f0826cafb2c8702b44be10f1b4ffce39e0686
piersto <pstoiko@gmail.com>
2020-10-16 15:54:40 -0400

    Finished 4.10: Сортировка списков перед сравнением. Вернул test_modify_first_group_header который падает. Почему -- не знаю.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -13,3 +13,15 @@ def test_modify_first_group_name(app):
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
+
+def test_modify_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create_group(Group(name='Test'))
+    old_groups = app.group.get_group_list()
+    group = Group(header='New Header')
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
