e7c9e02359d6982149373955855311db99bd9d70
piersto <pstoiko@gmail.com>
2020-11-29 23:05:33 -0500

    Added asserts to test_modify_contact_middlename

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -19,12 +19,19 @@ def test_modify_contact_lastname(app):
 
 def test_modify_contact_middlename(app):
     if app.contact.count() == 0:
-        app.contact.fill_contact_form(Contact('Contact to be modified'))
-    app.contact.modify_first_contact(Contact(middlename='II', birthday='15'))
+        app.contact.fill_in_contact_form(Contact(firstname='Contact to be modified'))
+    list_of_contacts_old = app.contact.list_of_contacts()
+    contact = Contact(middlename='II', birthday='15')
+    contact.id = list_of_contacts_old[0].id
+    app.contact.modify_first_contact(contact)
+    list_of_contacts_new = app.contact.list_of_contacts()
+    assert len(list_of_contacts_old) == len(list_of_contacts_new)
+    list_of_contacts_old[0] = contact
+    assert sorted(list_of_contacts_old, key=Contact.id_or_max) == sorted(list_of_contacts_new, key=Contact.id_or_max)
 
 
 def test_modify_contact_birth_month(app):
     if app.contact.count() == 0:
-        app.contact.fill_contact_form(Contact('Contact to be modified'))
+        app.contact.fill_in_contact_form(Contact('Contact to be modified'))
     app.contact.modify_first_contact(Contact(birth_month='April'))
 
